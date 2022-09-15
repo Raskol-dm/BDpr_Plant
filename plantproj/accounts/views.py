7349382b87d2b82da57cd01f029528c7426b6c86
Dmitry <d.raskolotov@gmail.com>
2022-09-15 19:30:34 +0300

    viewFirst

--- plantproj/accounts/views.py
+++ plantproj/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from accounts.models import Plants,Room
+from .filters import OrderFilter
 
 # Функции и классы, активирующие наши шаблоны URL
 # Create your views here.
@@ -19,8 +20,21 @@ def room(request):
     return render(request, 'room.html')
 
 
+# #Figts
+# def sportsmen(request):
+
+#     # figters = Fights.objects.using("user").filter
+#     figters = Sportsmen.objects.all()
+#     myFilter = OrderFilter(request.GET, queryset=figters)
+#     figters = myFilter.qs
+#     return render(request, 'accounts/sportsBD.html', {'figters': figters, 'myFilter': myFilter})
+
 def plants(request):
-    return render(request, 'plantsBD.html')
+    plants = Plants.objects.all()
+    myFilter = OrderFilter(request.GET, queryset=plants)
+    plants = myFilter.qs
+    print(plants)
+    return render(request, 'plantsBD.html', {'plants': plants, 'myFilter': myFilter})
 
 
 def history(request):
